eb6dd7b973a39d325bc153135c6c6cf5a9904b8b
Krati Jain <72783120+iamkrati22@users.noreply.github.com>
2022-04-03 08:26:32 +0530

    Updated Code

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,11 @@ import 'package:attendance_admin/login.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter_nearby_connections/flutter_nearby_connections.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -29,11 +34,7 @@ class _MyHomePageState extends State<MyHomePage> {
   final Future<SharedPreferences> prefs = SharedPreferences.getInstance();
 
   onClick() {
-    setState(() {
-      buticon = const Icon(Icons.bluetooth_connected);
-      button = 'Collecting';
-      text = 'Collecting Attendance Data, please wait';
-    });
+    _onButtonClicked(devices.first);
   }
 
   logout() async {
@@ -42,6 +43,113 @@ class _MyHomePageState extends State<MyHomePage> {
     prefs.remove('email');
     Navigator.pushReplacement(context,
         MaterialPageRoute(builder: (BuildContext ctx) => const Login()));
+  }
+
+  List<Device> devices = [];
+  List<Device> connectedDevices = [];
+  late NearbyService nearbyService;
+  late StreamSubscription subscription;
+  late StreamSubscription receivedDataSubscription;
+
+  bool isInit = false;
+
+  @override
+  void initState() {
+    super.initState();
+    init();
+  }
+
+  @override
+  void dispose() {
+    subscription.cancel();
+    receivedDataSubscription.cancel();
+    nearbyService.stopBrowsingForPeers();
+    nearbyService.stopAdvertisingPeer();
+    super.dispose();
+  }
+
+  // _onTabItemListener(Device device) async {
+  //   SharedPreferences prefs = await SharedPreferences.getInstance();
+  //   final erno = prefs.getString('erno');
+  //   if (device.state == SessionState.connected) {
+  //     nearbyService.sendMessage(device.deviceId, erno!);
+  //   }
+  // }
+
+  int getItemCount() {
+    return connectedDevices.length;
+  }
+
+  _onButtonClicked(Device device) {
+    switch (device.state) {
+      case SessionState.notConnected:
+        nearbyService.invitePeer(
+          deviceID: device.deviceId,
+          deviceName: device.deviceName,
+        );
+        break;
+      case SessionState.connected:
+        // nearbyService.disconnectPeer(deviceID: device.deviceId);
+        break;
+      case SessionState.connecting:
+        break;
+    }
+  }
+
+  void init() async {
+    nearbyService = NearbyService();
+    String devInfo = '';
+    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    if (Platform.isAndroid) {
+      AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+      devInfo = androidInfo.model!;
+    }
+    if (Platform.isIOS) {
+      IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
+      devInfo = iosInfo.localizedModel!;
+    }
+    await nearbyService.init(
+        serviceType: 'mpconn',
+        deviceName: devInfo,
+        strategy: Strategy.P2P_STAR,
+        callback: (isRunning) async {
+          if (isRunning) {
+            await nearbyService.stopAdvertisingPeer();
+            await nearbyService.stopBrowsingForPeers();
+            await Future.delayed(Duration(microseconds: 200));
+            await nearbyService.startAdvertisingPeer();
+            await nearbyService.startBrowsingForPeers();
+          }
+        });
+    subscription =
+        nearbyService.stateChangedSubscription(callback: (devicesList) {
+      devicesList.forEach((element) {
+        print(
+            " deviceId: ${element.deviceId} | deviceName: ${element.deviceName} | state: ${element.state}");
+
+        if (Platform.isAndroid) {
+          if (element.state == SessionState.connected) {
+            nearbyService.stopBrowsingForPeers();
+            ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text(element.deviceId + element.deviceName)));
+          } else {
+            nearbyService.startBrowsingForPeers();
+          }
+        }
+      });
+      devices.clear();
+      devices.addAll(devicesList);
+      connectedDevices.clear();
+      connectedDevices.addAll(
+          devicesList.where((d) => d.state == SessionState.connected).toList());
+    });
+
+    receivedDataSubscription =
+        nearbyService.dataReceivedSubscription(callback: (data) {
+      print("dataReceivedSubscription: ${jsonEncode(data)}");
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text(jsonEncode(data))));
+    });
   }
 
   @override
